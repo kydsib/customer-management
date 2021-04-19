8b0862be695a1f55ba427eb6b207b60d9beb4836
kydsib <simonas.kersulis@gmail.com>
2021-04-19 22:51:02 +0300

    Need to add tests for UserList

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 <img width="815" alt="Screenshot" src="https://user-images.githubusercontent.com/43815295/114357793-d5c98000-9b7a-11eb-8ff5-f12fb2cc6ce2.png">
 </details>
 
+<details>
 <summary>Completed task</summary>
 <img width="815" alt="Screenshot" src="https://user-images.githubusercontent.com/43815295/115288160-c8535d80-a159-11eb-922e-0cef1c8ce2ed.gif">
 </details>
--- src/components/UsersList/test/UsersList.test.jsx
+++ src/components/UsersList/test/UsersList.test.jsx
@@ -7,25 +7,7 @@ describe('<UsersList />', () => {
 		render(<UsersList />)
 		expect(screen.getByText(/there are no users yet/i)).toBeInTheDocument()
 	})
-
-	it('renders saved users', () => {
-		const useForm = jest.fn()
-
-		useForm.mockReturnValue({
-			city: 'Vilnius',
-			email: 'dsad@dasd.lt',
-			fullName: 'Testi',
-			houseNo: '22',
-			id: 1618846253028,
-			street: 'Kauno',
-			zip: '12',
-		})
-		// mock use from
-
-		render(<UsersList />)
-		const { validUsers } = useForm()
-		screen.debug()
-		// return validUsers from mocked useForm?
-		// test that data is rendered
-	})
 })
+
+// TODO need to test how user renders
+// TODO test when editing user
